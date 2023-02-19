9d09625991b77d973de8dfd18911d7abd8cb0e43
felipinas <jfss2@cin.ufpe.br>
2023-02-19 12:54:32 -0300

    feat: add pages in route and notion logo

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,14 +2,28 @@ import { useRoutes } from 'react-router-dom';
 
 import { DefaultLayout } from '../layouts/DefaultLayout';
 
+import { AboutMe } from '../view/AboutMe';
+import { Home } from '../view/Home';
+
 export function Routes() {
   const routes = useRoutes([
     {
       path: '/',
-      element: <DefaultLayout>home</DefaultLayout>,
+      element: (
+        <DefaultLayout>
+          <Home />
+        </DefaultLayout>
+      ),
     },
     { path: '/blog', element: <DefaultLayout>blog</DefaultLayout> },
-    { path: '/about-me', element: <DefaultLayout>about-me</DefaultLayout> },
+    {
+      path: '/about-me',
+      element: (
+        <DefaultLayout>
+          <AboutMe />
+        </DefaultLayout>
+      ),
+    },
   ]);
 
   return routes;
